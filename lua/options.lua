47814a7915696e6efeccea06af3deb9d1a793730
KGray <serykhk.u@gmail.com>
2022-06-10 10:07:25 +0300

    Replace the config language with lua

---
+++ lua/options.lua
@@ -0,0 +1,15 @@
+vim.opt.backup = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.mouse = "a"
+vim.opt.cmdheight = 2
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.swapfile = false
+vim.opt.undofile = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.cursorline = true
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.numberwidth = 4
